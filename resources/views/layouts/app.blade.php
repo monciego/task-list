72dd8a16c929c0f2284f2ae13f8916ff41b4a7b8
monciego <jerichobantiquete@gmail.com>
2024-01-25 21:48:31 +0800

    sessions, errors and flash messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Task List</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield("title")</h1>
 
-    @yield("content")
+    <div>
+        @if (session()->has('success'))
+        <div>{{ session('success') }}</div>
+        @endif
+        @yield("content")
+    </div>
 </body>
 
 </html>
