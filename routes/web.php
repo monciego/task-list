9bc397aa223c9622758c1fc9db22126e33b2af81
monciego <jerichobantiquete@gmail.com>
2024-01-25 21:37:31 +0800

    storing and validating data

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
     return redirect()->route("tasks.index");
 });
 
-
 Route::get('/tasks', function () {
     return view('index', [
         'tasks' => Task::latest()->get()
@@ -35,5 +34,17 @@ Route::get('/tasks/{id}', function ($id) {
 })->name('tasks.show');
 
 Route::post('tasks', function(Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        "title" => 'required|max:255',
+        "description" => 'required',
+        "long_description" => 'required',
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
