5f32cc708eb260352d2be8c1ff798fb11d0329d4
monciego <jerichobantiquete@gmail.com>
2024-01-25 22:52:16 +0800

    deleting data

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,19 @@
 @section('title', $task->title)
 
 @section("content")
-<p>{{ $task->created_at }}</p>
 <p>{{ $task->description }}</p>
+
+@if ($task->long_description)
 <p>{{ $task->long_description }}</p>
+@endif
+<p>{{ $task->created_at }}</p>
+<p>{{ $task->updated_at }}</p>
+
+<div>
+    <form method="POST" action="{{ route('tasks.destroy', $task) }}">
+        @csrf
+        @method("DELETE")
+        <button type="submit">Delete</button>
+    </form>
+</div>
 @endsection
